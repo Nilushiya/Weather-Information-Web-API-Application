f8a89f966093a71f0338cbe6d0b731373f427f74
Nilushiya <nilushiyak@gmail.com>
2025-08-22 21:42:55 +0530

    all changes because of lap issue

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,8 +1,54 @@
-
+import React, { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+import weatherIcon from './assets/cloudy.png';
+import './style/App.css';
+import { Button, Spinner } from 'react-bootstrap';
 function App() {
+const { loginWithRedirect, logout, isAuthenticated, getAccessTokenSilently } = useAuth0();
+const [weatherData, setWeatherData] = useState([]);
+const [loading, setLoading] = useState(false);
+
+// useEffect(() => {
+//     if (isAuthenticated) {
+//       fetchWeather();
+//     }
+//   }, [isAuthenticated]);
+useEffect(() => {
+    // if (isAuthenticated) {
+      fetchWeather();
+    // }
+  }, []);
+  const fetchWeather = async () => {
+    setLoading(true);
+    try {
+      // const token = await getAccessTokenSilently();
+      const response = await axios.get('http://localhost:8080/api/weather'
+      //   , {
+      //   headers: { Authorization: `Bearer ${token}` }
+      // }
+    );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error fetching weather:', error);
+    }
+    setLoading(false);
+  };
+
+// if(!isAuthenticated){
+//   return(
+//     <div class="container">
+//       <h1>Weather App</h1>
+//       <Button onClick={() => loginWithRedirect()}>Log In</Button>
+//     </div>
+//   )
+// }
   return (
-    <div className="App">
-      Hi
+    <div class=" app" >
+      {/* <div className='shadow'></div> */}
+      <h1 ><img src={weatherIcon} alt="Weather Icon" />Weather App</h1>
+      {/* <div id='cloud'></div> */}
+      {/* <Button variant="danger" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</Button> */}
     </div>
   );
 }
--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    // domain="dev-h47an2orx06cwhog.us.auth0.com"
+    // clientId="dSrlQwa5pJw9NmwYUMauDNxgPqiXMCpp"
+    // authorizationParams={{
+    //   redirect_uri: window.location.origin
+    // }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
