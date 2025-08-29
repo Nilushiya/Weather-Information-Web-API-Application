935cdc09a05824f226817af4694bcdf2819de681
Nilushiya <nilushiyak@gmail.com>
2025-08-29 21:37:58 +0530

    Frontend some format modifications

--- weather-app/src/component/WeatherDetail.jsx
+++ weather-app/src/component/WeatherDetail.jsx
@@ -1,32 +1,50 @@
-import React from 'react'
-import { useLocation, useNavigate} from "react-router-dom";
-import '../style/weatherDetail.css';
-import weatherIcon from '../assets/cloud.png';
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "../style/weatherDetail.css";
+import weatherIcon from "../assets/cloud.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import cloudBack from "../assets/clu2.png";
 
 const WeatherDetail = () => {
-    const { state } = useLocation();
-    const navigate = useNavigate();
-    const weather = state?.data;
-
-    const handleBack = () => {
-     navigate("/weather");
-    }
+  const { state } = useLocation();
+  const navigate = useNavigate();
+  const weather = state?.data;
+  const color = state?.color;
+  console.log("color:", color);
+  const handleBack = () => {
+    navigate("/weather");
+  };
   return (
-    <div className='weatherdetail'>
+    <div className="weatherdetail">
       <header className="weatherdetail-header">
-        <h5><img src={weatherIcon} alt="Weather Icon" style={{ width: '50px', height: '35px' }}/>Weather App</h5>
-      </header>
-      <div className='weatherdetail-card'>
-        <div className="row-card">
-          <FontAwesomeIcon 
-            icon={faArrowLeft} 
-            onClick={handleBack} 
-            style={{ cursor: "pointer", color:"black"}}
-            className='arrow'
+        <h5>
+          <img
+            src={weatherIcon}
+            alt="Weather Icon"
+            style={{ width: "50px", height: "35px" }}
           />
-          <div className='row1'>
+          Weather App
+        </h5>
+      </header>
+      <div className="weatherdetail-card">
+        <div
+          className="row-card"
+          style={{
+            backgroundImage: `linear-gradient(${color}, ${color}), url(${cloudBack})`,
+            backgroundPosition: "center bottom",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundBlendMode: "overlay",
+          }}
+        >
+          <FontAwesomeIcon
+            icon={faArrowLeft}
+            onClick={handleBack}
+            style={{ cursor: "pointer", color: "black" }}
+            className="arrow"
+          />
+          <div className="row1">
             <h5>{weather.cityName}</h5>
             <h2>{weather.temp}°C</h2>
           </div>
@@ -36,7 +54,7 @@ const WeatherDetail = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WeatherDetail
+export default WeatherDetail;
--- weather-app/src/page/Weather.jsx
+++ weather-app/src/page/Weather.jsx
@@ -5,13 +5,23 @@ import { Button, Form, Row, Col } from "react-bootstrap";
 import weatherIcon from '../assets/cloud.png';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from "react-router-dom";
+import cloudBack from "../assets/clu2.png";
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [city, setCity] = useState();
   const {getAccessTokenSilently, loginWithRedirect, logout, isAuthenticated, isLoading} = useAuth0();
-  const cardColors = ["#6f42c1", "#198754", "#0dcaf0", "#ffc107", "#dc3545", "#fd7e14", "#f53bf5ff", "#f70e6fff"];
+  const cardColors = [
+    "#6f42c1",
+    "#03859cff",
+    "#ca9905ff",
+    "#c04854d8",
+    "#0bac18e1",
+    "#d12e2eff",
+    "#2c29ceff",
+    "#696b04ff",
+  ];
   const navigate = useNavigate();
 
   useEffect(() =>{
@@ -33,11 +43,11 @@ const Weather = () => {
 
     try {
       const token = await getAccessTokenSilently({
-      audience: "https://myapi.example.com",
-    });
-    console.log("token: ",token);
-    const response = await axios.get("http://localhost:8080/api/weather", {
-      headers: { Authorization: `Bearer ${token}` }
+        audience: "https://myapi.example.com",
+      });
+      console.log("token: ",token);
+      const response = await axios.get("http://localhost:8080/api/weather", {
+        headers: { Authorization: `Bearer ${token}` }
     }
     );
       setWeatherData(response.data.data);
@@ -53,9 +63,23 @@ const Weather = () => {
   return (
     <>
       <div className="app">
-        <Button className='logout' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</Button>
+        <Button
+          className="logout"
+          onClick={() =>
+            logout({ logoutParams: { returnTo: window.location.origin } })
+          }
+        >
+          Log Out
+        </Button>
         <header className="app-header">
-          <h5><img src={weatherIcon} alt="Weather Icon" style={{ width: '50px', height: '35px' }}/>Weather App</h5>
+          <h5>
+            <img
+              src={weatherIcon}
+              alt="Weather Icon"
+              style={{ width: "50px", height: "35px" }}
+            />
+            Weather App
+          </h5>
           <div className="search-bar">
             <Form.Control
               type="text"
@@ -70,25 +94,41 @@ const Weather = () => {
         </header>
 
         <Row className="cards-grid m-0 p-0">
-           {loading ? (
-              <div className="loading-container">
-                <div className="spinner"></div>
-              </div>
-            ) : (weatherData && weatherData.map((c, index) => (
-            <Col key={c.cityName} xs={12} md={6} lg={6}>
-              <div className='weather-card'
-                onClick={() => navigate(`/weather/${c.cityName}`, { state: { data: c } })}
-                style={{ cursor: "pointer", backgroundColor: cardColors[index % cardColors.length] }}>
-                <div className="row-one" style={{ backgroundColor: cardColors[index % cardColors.length] }}>
-                  <h5>{c.cityName}</h5>
-                  <h2>{c.temp}°C</h2>
+          {loading ? (
+            <div className="loading-container">
+              <div className="spinner"></div>
+            </div>
+          ) : (
+            weatherData &&
+            weatherData.map((c, index) => (
+              <Col key={c.cityName} xs={12} md={6} lg={6}>
+                <div
+                  className="weather-card"
+                  onClick={() =>
+                    navigate(`/weather/${c.cityName}`, { state: { data: c, color:cardColors[index % cardColors.length]} })
+                  }
+                  style={{
+                    cursor: "pointer",}}
+                >
+                  <div
+                    className="row-one"
+                    style={{
+                      background: `${
+                        cardColors[index % cardColors.length]
+                      } url(${cloudBack}) bottom center / cover no-repeat `,
+                      backgroundBlendMode: "overlay"
+                    }}
+                  >
+                    <h5>{c.cityName}</h5>
+                    <h2>{c.temp}°C</h2>
+                  </div>
+                  <div className="row-two">
+                    <p>{c.weatherDescription}</p>
+                  </div>
                 </div>
-                <div className="row-two">
-                  <p>{c.weatherDescription}</p>
-                </div>
-              </div>
-            </Col>
-          )))}
+              </Col>
+            ))
+          )}
         </Row>
       </div>
 
@@ -96,7 +136,7 @@ const Weather = () => {
         <p>2025 Fidenz Technologies</p>
       </footer>
     </>
-  )
-}
+  );
+};
 
-export default Weather
+export default Weather;
